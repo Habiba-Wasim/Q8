7a0064d2f4fb28bfc3fdc41988cb836352dcf7ac
Habiba-Wasim <160347457+Habiba-Wasim@users.noreply.github.com>
2024-02-23 21:00:43 +0500

    Step8 Of Typescript
    Step8 Of Typescript

---
+++ Q8.js
@@ -0,0 +1,9 @@
+"use strict";
+//You should create four lines that look like this:
+//console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+// 8.
+console.log(5 + 3);
+console.log(18 - 10);
+console.log(2 * 4);
+console.log(16 / 2);
